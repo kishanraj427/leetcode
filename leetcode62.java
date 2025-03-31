d56f47eda8cf443ec44e2208d46a9eb3393bbd93
kishanraj <kishanraj492000@gmail.com>
2025-03-31 22:48:53 +0530

    Added code for leetcode problem 62

---
+++ leetcode62.java
@@ -0,0 +1,22 @@
+// 62. Unique Paths
+
+/*  
+Runtime 0ms -> Beats 100.00%
+Memory 40.55MB -> Beats 46.61%
+*/
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m+1][n+1];
+        for(int i = 0; i <= m; i++) dp[i][1] = 1;
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                int a = 0, b = 0;
+                a = dp[i-1][j];
+                b = dp[i][j-1];
+                dp[i][j] = a + b;
+            }
+        }
+        return dp[m][n];
+    }
+}

7878f2dbf25991b38c96753d1d9ad7a433bddd9e
kishanraj <kishanraj492000@gmail.com>
2025-04-04 16:30:43 +0530

    Added 121

---
+++ leetcode121 Best Time to Buy and Sell Stock.java
@@ -0,0 +1,34 @@
+// 121. Best Time to Buy and Sell Stock
+
+/*  
+Runtime 1ms -> Beats 99.82%%
+Memory 61.62MB -> Beats 63.53%
+*/
+
+import java.util.*;
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> tri) {
+        int n = tri.size();
+        int[][] dp = new int[n][n];
+        dp[0][0] = tri.get(0).get(0);
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < i+1; j++) {
+                int a = tri.get(i).get(j);
+                int b = 10000, c = 10000;
+                if(j != 0) b = dp[i-1][j-1];
+                if(i != j) c = dp[i-1][j];
+                dp[i][j] = a + Math.min(b, c);
+            }   
+        }
+        return getMin(dp[n-1]);
+    }
+
+    int getMin(int[] list) {
+        int min = list[0];
+        for(int i = 1; i < list.length; i++) {
+            if(min > list[i]) min = list[i];
+        }
+        return min;
+    }
+}

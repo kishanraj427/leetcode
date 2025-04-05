d5ed39ea415f45f169325cad84f39156970da43a
kishanraj <kishanraj492000@gmail.com>
2025-04-05 18:04:38 +0530

    added 131

---
+++ leetcode131 Palindrome Partitioning.java
@@ -0,0 +1,43 @@
+// 131. Palindrome Partitioning
+
+/*  
+8ms Beats -> 54.76%
+Memory 57.00MB -> Beats 59.00%
+
+*/
+
+import java.util.*;
+
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        List<String> store = new ArrayList<>();
+        doWork(s, result, store, 0);
+        return result;
+    }
+
+    void doWork(String s, List<List<String>> result, List<String> store, int ind){
+        if(ind == s.length()) {
+            result.add(new ArrayList<>(store));
+            return;
+        }
+        for(int i = ind; i < s.length(); i++) {
+            if(isPalindrome(s, ind, i)) {
+                store.add(s.substring(ind, i+1));
+                doWork(s, result, store, i+1);
+                store.remove(store.size()-1);
+            }
+        }
+    }
+
+     boolean isPalindrome(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}

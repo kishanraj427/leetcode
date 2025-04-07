897371568213c66b1dccf92fc6e8f39d4c59c62a
kishanraj <kishanraj492000@gmail.com>
2025-04-07 23:52:36 +0530

    Updated 338

--- leetcode338 Counting Bits.java
+++ leetcode338 Counting Bits.java
@@ -1,19 +1,26 @@
 // 338. Counting Bits
 
 /*  
-0ms Beats -> 100%
-Memory 41.16MB -> Beats 49.49%
+3ms Beats -> 34.53%
+Memory 46.68MB -> Beats 67.43%
 */
 
+import java.util.*;
 class Solution {
     public int[] countBits(int n) {
         int[] store = new int[n+1];
+        Arrays.fill(store, -1);
         for(int i = 0; i <= n; i++) {
             int count = 0;
             int num = i;
             while(num != 0) {
-                if(num % 2 == 1) count++;
-                num = num / 2;
+                if(store[num] == -1) {
+                    if(num % 2 == 1) count++;
+                    num = num / 2;
+                } else {
+                    count += store[num];
+                    break;
+                }
             }   
             store[i] = count; 
         } 

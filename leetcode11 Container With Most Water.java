bc690cd6159646faa6846c7310bb90c2df3fe869
kishanraj <kishanraj492000@gmail.com>
2025-04-15 23:09:55 +0530

    Add 11

---
+++ leetcode11 Container With Most Water.java
@@ -0,0 +1,27 @@
+// 11. Container With Most Water
+
+/*  
+5ms Beats -> 74.76%%
+Memory 57.81MB -> Beats 64.60%%
+
+*/
+
+class Solution {
+    public int maxArea(int[] height) {
+        int n = height.length-1;
+        int i = 0;
+        int max = 0;
+        while(i < n) {
+            int l = 0;
+            if(height[i] < height[n]) {
+                l = height[i];
+                i++;
+            } else {
+                l = height[n];
+                n--;
+            }
+            max = Math.max(max, l * (n-i+1));
+        }
+        return max;
+    }
+}

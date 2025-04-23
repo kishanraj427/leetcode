0f742e91d9b76541a4d40d40d5b1ec936b7fc98c
kishanraj <kishanraj492000@gmail.com>
2025-04-23 22:39:50 +0530

    added 753

---
+++ leetcode753 Cracking the Safe.java
@@ -0,0 +1,42 @@
+// 753. Cracking the Safe
+
+/*  
+14ms Beats -> 68.34%
+Memory 46.02MB -> Beats 12.56%
+*/
+
+import java.util.*;
+
+class Solution {
+    public String crackSafe(int n, int k) {
+        StringBuilder sb = new StringBuilder();
+        int total = (int) (Math.pow(k, n));
+        for (int i = 0; i < n; i++) sb.append('0');
+
+        Set<String> visited = new HashSet<>();
+        visited.add(sb.toString());
+
+        dfs(sb, total, visited, n, k);
+
+        return sb.toString();
+    }
+
+    private boolean dfs(StringBuilder sb, int goal, Set<String> visited, int n, int k) {
+        if (visited.size() == goal) return true;
+        String prev = sb.substring(sb.length() - n + 1, sb.length());
+        for (int i = 0; i < k; i++) {
+            String next = prev + i;
+            if (!visited.contains(next)) {
+                visited.add(next);
+                sb.append(i);
+                if (dfs(sb, goal, visited, n, k)) return true;
+                else {
+                    visited.remove(next);
+                    sb.delete(sb.length() - 1, sb.length());
+                }
+            }
+        }
+        return false;
+    }
+
+}
